9af712428ba54b8eb4bfeae2458b3e433f020b62
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-09 00:30:47 +0600

    data-loaded

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,11 +16,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rjzdusk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+
+  await client.connect()
+  const serviceCollection = client.db('take-a-trip').collection('services');
+  try {
+      app.get('/services', async (req, res) => {
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+      });
+  }
+  finally {
+
+  }
+}
+run().catch(err => console.error(err));
 
 
 
