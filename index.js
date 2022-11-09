5deca6341b0733f827b1c7176dc2549f0dfe49d1
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-09 22:01:51 +0600

    limit-added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
       app.get('/services', async (req, res) => {
           const query = {}
           const cursor = serviceCollection.find(query);
-          const services = await cursor.toArray();
+          const services = await cursor.limit(3).toArray();
           res.send(services);
       });
   }
