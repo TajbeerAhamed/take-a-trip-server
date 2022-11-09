b88149ff37dce25323cd284f4d6bad20f24c64ba
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-11-09 23:28:50 +0600

    product-details-server-added

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run() {
           const services = await cursor.limit(3).toArray();
           res.send(services);
       })
+      app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const cursor = await serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      });
   }
   finally {
 
